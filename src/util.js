8b4dbbaa24047219fa0f4a0cafec1949180e30b7
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-23 17:53:13 +1100

    changed the timer

--- src/util.js
+++ src/util.js
@@ -6,12 +6,13 @@ module.exports.parseTime = (string) => {
   if (timeArray.length !== 3)
     return 'Error';
 
-  let hour = parseInt(timeArray[0]) * 60 * 60 * 10000;
-  let min = parseInt(timeArray[1]) * 60 * 1000;
+  let hour = parseInt(timeArray[0]) * 3600000
+  let min = parseInt(timeArray[1]) * 60000;
   let sec = parseInt(timeArray[2]) * 1000;
 
   return hour + min + sec;
 }
+
 module.exports.timeUp = (args, msg) => {
   const embed = new Discord.MessageEmbed()
     .setColor('#606e8c')
